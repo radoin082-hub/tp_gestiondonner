641b2929f074629849671eee401f76a466d4037b
radoin082-hub <radoune.rachedi@univ-biskra.dz>
2024-10-24 00:19:13 +0100

    first page

--- BlazorApp1/Services/ProductServiceq.cs
+++ BlazorApp1/Services/ProductServiceq.cs
@@ -1,99 +1,127 @@
 ﻿using System.Xml.Linq;
-using System.IO;
+
 using BlazorApp1.Models;
-using System.Xml;
-using System.Linq;
-using System;
-using DotNetEnv;
+
+
 namespace BlazorApp1.Services
 {
     public class ProductService
     {
-        public ProductService()
+     
+
+        public List<Product> ReadProducts(string path)
         {
-          
-        }
-
-
-
-
-        public List<Product> readProducts(String path)
-        {
-            List<Product> products=new List<Product>();
+            List<Product> products = new List<Product>();
             try
             {
+                
                 XDocument xmlDoc = XDocument.Load(path);
-
                 var productsXML = xmlDoc.Element("products")?.Elements("product");
 
-                foreach(var product in productsXML)
+              
+                if (productsXML != null)
                 {
-                   String id=  product.Element("id")?.Value;
-                    String name = product.Element("name")?.Value;
-                    String description = product.Element("description")?.Value;
-                    String price = product.Element("price")?.Value;
-                    String category = product.Element("category")?.Value;
-                    String stock = product.Element("stock")?.Value; 
-                    products.Add(new Product(name,id, description, price, category, stock));
+                    foreach (var product in productsXML)
+                    {
+                
+                        string id = product.Element("id")?.Value;
+                        string name = product.Element("name")?.Value;
+                        string description = product.Element("description")?.Value;
+                        string price = product.Element("price")?.Value;
+                        string category = product.Element("category")?.Value;
+                        string stock = product.Element("stock")?.Value;
+
+               
+                        products.Add(new Product(name, id, description, price, category, stock));
+                    }
                 }
-
-
-
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-
             }
-            
             return products;
         }
 
-        public void addProduct(string path,Product product)
+        public void AddProduct(string path, Product product)
         {
             try
             {
                 XDocument xmlDoc = XDocument.Load(path);
-
                 var productsXML = xmlDoc.Element("products");
-             XElement newproducts= new XElement("product", new XElement("id", product.id),new XElement("name",product.name));
-                productsXML.Add(newproducts);
-                xmlDoc.Save(path);
 
-                Console.WriteLine("hi");
-                    }
+                XElement newProduct = new XElement("product",
+                    new XElement("id", product.id),
+                    new XElement("name", product.name),
+                    new XElement("description", product.description),
+                    new XElement("price", product.price),
+                    new XElement("category", product.category),
+                    new XElement("stock", product.stock));
+
+                
+                productsXML?.Add(newProduct);
+                xmlDoc.Save(path);
+                     }
             catch (Exception e)
             {
-
-            }
+                 }
         }
 
-
-
-        public void deleteProduct(string path, Product product)
+        public void UpdateProduct(string path, Product updatedProduct)
         {
             try
             {
                 XDocument xmlDoc = XDocument.Load(path);
+                var productToUpdate = xmlDoc.Descendants("product")
+                    .FirstOrDefault(p => (string)p.Element("id") == updatedProduct.id);
 
+              
+                if (productToUpdate != null)
+                {
+   
+                    productToUpdate.Element("name").Value = updatedProduct.name;
+                    productToUpdate.Element("description").Value = updatedProduct.description;
+                    productToUpdate.Element("price").Value = updatedProduct.price;
+                    productToUpdate.Element("category").Value = updatedProduct.category;
+                    productToUpdate.Element("stock").Value = updatedProduct.stock;
 
-
-                var productsXML = xmlDoc.Descendants("product").FirstOrDefault(p => (string)p.Element("id").Value == product.id);
-                productsXML.SetValue("1");
-               /* productsXML.Remove();*/
-                xmlDoc.Save(path);
-             
-
+                    xmlDoc.Save(path);
+                       }
+                else
+                {
+                    Console.WriteLine("null");
+                }
             }
             catch (Exception e)
             {
-
+                Console.WriteLine(e.Message);
             }
         }
 
-       
+        public void DeleteProduct(string path, Product product)
+        {
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(path);
+                var productsXML = xmlDoc.Descendants("product")
+                    .FirstOrDefault(p => (string)p.Element("id") == product.id);
 
-
+               
+                if (productsXML != null)
+                {
+                    productsXML.Remove();
+                    xmlDoc.Save(path);
+                   
+                }
+                else
+                {
+                    Console.WriteLine("null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
-
