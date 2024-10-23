63a47fbda45194677ccbc526b338128b7bac5831
Dali_Raki <Dali_Raki@DESKTOP-KJDD8OP>
2024-10-23 14:50:04 +0200

    Add project files.

---
+++ BlazorApp1/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Product
+    {
+        public String id { get; set; }
+        public String name {  get; set; }
+        public String description { get; set; }
+        public String price { get; set; }
+        public String category { get; set; }
+        public String stock { get; set; }
+
+        public Product(String _name , String _id,   String _description, String _price, String _category, String _stock) { 
+            this.name = _name;
+            this.id = _id;
+            this.description = _description;
+            this.price = _price;
+            this.category = _category;
+            this.stock = _stock;
+
+        
+        }
+
+    }
+}
---
+++ BlazorApp1/Services/ProductServiceq.cs
@@ -0,0 +1,98 @@
+﻿using System.Xml.Linq;
+using System.IO;
+using BlazorApp1.Models;
+using System.Xml;
+using System.Linq;
+using System;
+namespace BlazorApp1.Services
+{
+    public class ProductService
+    {
+
+
+      
+  
+
+        public List<Product> readProducts(String path)
+        {
+            List<Product> products=new List<Product>();
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(path);
+
+                var productsXML = xmlDoc.Element("products")?.Elements("product");
+
+                foreach(var product in productsXML)
+                {
+                   String id=  product.Element("id")?.Value;
+                    String name = product.Element("name")?.Value;
+                    String description = product.Element("description")?.Value;
+                    String price = product.Element("price")?.Value;
+                    String category = product.Element("category")?.Value;
+                    String stock = product.Element("stock")?.Value; 
+                    products.Add(new Product(name,id, description, price, category, stock));
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            
+            return products;
+        }
+
+        public void addProduct(string path,Product product)
+        {
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(path);
+
+                var productsXML = xmlDoc.Element("products");
+             XElement newproducts= new XElement("product", new XElement("id", product.id),new XElement("name",product.name));
+                productsXML.Add(newproducts);
+                xmlDoc.Save(path);
+
+                Console.WriteLine("hi");
+                    }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+
+        public void editProduct(string path, Product product)
+        {
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(path);
+
+
+
+                var productsXML = xmlDoc.Descendants("product").FirstOrDefault(p => (string)p.Element("id") =="5");
+
+                /*   productsXML.Element("id")?.SetValue("5");*/
+                productsXML.Remove();
+             
+
+                xmlDoc.Save(path);
+
+                Console.WriteLine("zeb");
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+       
+
+
+    }
+}
+
